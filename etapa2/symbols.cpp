e2987cab78c44bc75dbf12ac8f79b20fa4cc37ed
Breno Morais <bneromorais@gmail.com>
2025-09-06 12:26:44 -0300

    Etapa 2 feito, ainda com muito shift conflicts

---
+++ etapa2/symbols.cpp
@@ -0,0 +1,61 @@
+// Trabalho Etapa 1 - Compiladores
+// Aluno: Breno da Silva Morais - 00335794
+
+#include "symbols.h"
+
+#include <map>
+#include <iostream>
+
+std::map<std::string, Symbol> SymbolsTable;
+
+int insertSymbolIntoTable(char* text, int token) {
+    std::string symbol = std::string(text);
+
+    if(SymbolsTable.find(symbol) != SymbolsTable.end())
+        return token;
+
+    SymbolType type;
+    switch (token) {
+        case TK_IDENTIFIER:
+        case LIT_INT:
+        case LIT_CHAR:
+        case LIT_FLOAT:
+        case LIT_STRING:
+            type = static_cast<SymbolType>(token);
+            break;
+        
+        default:
+            return TOKEN_ERROR;
+            break;
+    }
+
+    SymbolsTable[symbol] = Symbol{type, symbol};
+    return token;
+}
+
+static std::string getTypeString(const SymbolType& value) {
+    #define ADD_NAME(event) {event, #event}
+    static const std::map<const SymbolType, std::string> result {
+        ADD_NAME(SymbolType::Identifier),
+        ADD_NAME(SymbolType::Integer),
+        ADD_NAME(SymbolType::Char),
+        ADD_NAME(SymbolType::Float),
+        ADD_NAME(SymbolType::String),
+    };
+    #undef ADD_NAME
+    auto it = result.find(value);
+    if (it == result.end()) return "Unknown Type";
+    return it->second;
+};
+
+std::ostream& operator<<(std::ostream& out, const SymbolType& value) {
+    return out << getTypeString(value);
+};
+
+void printSymbolsTable() {
+    std::cout << "Symbol Table:\n";
+    for (const auto& entry : SymbolsTable) {
+        std::cout << "  [" << entry.first
+                  << ", " << entry.second.type << "]\n";
+    }
+}
---
+++ etapa2/symbols.h
@@ -0,0 +1,29 @@
+#ifndef COMP_SYM
+#define COMP_SYM
+
+#include "parser.tab.hpp"
+
+#include <string>
+#include <stdint.h>
+
+enum class SymbolType: uint16_t {
+    Identifier = TK_IDENTIFIER,
+    Integer = LIT_INT,
+    Char = LIT_CHAR,
+    Float = LIT_FLOAT,
+    String = LIT_STRING,
+};
+
+// still not sure if the map should have the content on the value, because it's always the same as the key, it feels redudant
+
+struct Symbol {
+    SymbolType type;
+    std::string content;
+};
+
+int insertSymbolIntoTable(char* text, int type);
+void printSymbolsTable();
+
+std::ostream& operator<<(std::ostream& out, const SymbolType& value);
+
+#endif /* COMP_SYM */
