c25af4345b0c811b07671a3daa0bf7618d1ac728
Breno Morais <bneromorais@gmail.com>
2025-10-03 16:04:57 -0300

    start of etapa 4

---
+++ etapa4/symbols.h
@@ -0,0 +1,28 @@
+#ifndef COMP_SYM
+#define COMP_SYM
+
+#include <string>
+#include <stdint.h>
+
+enum class SymbolType: uint16_t {
+    Identifier,
+    Integer,
+    Char,
+    Float,
+    String,
+    Bool,
+};
+
+// still not sure if the map should have the content on the value, because it's always the same as the key, it feels redudant
+
+struct Symbol {
+    SymbolType type;
+    std::string content;
+};
+
+Symbol* insertSymbolIntoTable(char* text, SymbolType type);
+void printSymbolsTable();
+
+std::ostream& operator<<(std::ostream& out, const SymbolType& value);
+
+#endif /* COMP_SYM */
